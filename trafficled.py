6dffd78f3e120de9fc965f2c4854483596a8d262
Derek Shaw <derek@soslug.org>
2014-09-22 11:10:57 +0100

    File updated to use pattern arrays

--- trafficled.py
+++ trafficled.py
@@ -19,6 +19,14 @@ set_gpio()
 
 from array import *
 gpio_array = array('i',[3,5,7,8,10,12,19,21,22,23,24,26])
+pattern1 = array('i',[3,8,23,26])
+pattern2 = array('i',[8,23])
+pattern3 = array('i',[5,10,21,24])
+pattern4 = array('i',[3,5,10,21,24,26])
+pattern5 = array('i',[7,12,19,22])
+pattern6 = array('i',[7,22])
+pattern7 = array('i',[5,10,12,19,21,24])
+ 
 
 for i in gpio_array:
 	GPIO.setup(i, GPIO.OUT)
@@ -27,65 +35,62 @@ for i in gpio_array:
 time.sleep(8)
 
 while True:
-	
+	# Pattern1	
 	# Turn on the Red LED pinout 3 & 26
-	# Red light stays on
-	GPIO.output(3, GPIO.HIGH)
-	GPIO.output(26, GPIO.HIGH)
 	# Turn on the Green LED pinout 8 & 23
-	GPIO.output(8, GPIO.HIGH)
-	GPIO.output(23, GPIO.HIGH)
-	time.sleep(4)
-	# Turn off Green LEDs pinout 8 & 23
-	GPIO.output(8, GPIO.LOW)
-	GPIO.output(23, GPIO.LOW)
-	# Turn on the Yellow LED pinout 5, 10, 21 & 24
-	# Yellow light on with Red
- 	GPIO.output(5, GPIO.HIGH)
- 	GPIO.output(10, GPIO.HIGH)
- 	GPIO.output(21, GPIO.HIGH)
-	GPIO.output(24, GPIO.HIGH)
-	time.sleep(2)
-	# Turn off the Yellow LED pinout 5, 10, 21 & 24
-	# Red and Yellow lights off
-	GPIO.output(5, GPIO.LOW)
-	GPIO.output(10, GPIO.LOW)
-	GPIO.output(21, GPIO.LOW)
- 	GPIO.output(24, GPIO.LOW)
-	# Turn off the Red LED pinout 3 & 26
-	GPIO.output(3, GPIO.LOW)
-	GPIO.output(26, GPIO.LOW)
+	for i in pattern1:
+		GPIO.output(i, GPIO.HIGH)
 
-	# Turn on Green lights
-	# Turn off the Green LED pinout 8 & 23
-	GPIO.output(8, GPIO.LOW)
-	GPIO.output(23, GPIO.LOW)
-	# Turn on the Green LED pinout 7 & 22
- 	GPIO.output(7, GPIO.HIGH)
-	GPIO.output(22, GPIO.HIGH)
-	# Turn on the Red LED pinout 12 & 19
-	GPIO.output(12, GPIO.HIGH)
-	GPIO.output(19, GPIO.HIGH)
 	time.sleep(4)
-	# Turn off the Green LED pinout 7 & 22
-	GPIO.output(7, GPIO.LOW)
-	GPIO.output(22, GPIO.LOW)
-	# Turn on the Yellow LED pinout 5 & 24
-	GPIO.output(5, GPIO.HIGH)
-	GPIO.output(24, GPIO.HIGH)
-	# Turn on the Yellow LED pinout 10 & 21
-	GPIO.output(10, GPIO.HIGH)
-	GPIO.output(21, GPIO.HIGH) 
+
+	# Pattern2
+	# Turn off Green LEDs pinout 8 & 23
+	for i in pattern2:
+		GPIO.output(i, GPIO.LOW)
+	
+	# Pattern3
+	# Turn on the Yellow LED pinout 5, 10, 21 & 24
+	# Yellow light on with Red 3, 26
+	for i in pattern3:
+		GPIO.output(i, GPIO.HIGH)
+ 	
 	time.sleep(2)
+
+	# Pattern4
+	# Turn off the Yellow LED pinout 2, 5, 10, 21, 24 & 26
+	# Red and Yellow lights off
+	for i in pattern4:
+		GPIO.output(i, GPIO.LOW)
+
+	# Pattern5
+	# Turn on the Green LED pinout 8 & 23
+	for i in pattern5:
+		GPIO.output(i, GPIO.HIGH)
+
+	time.sleep(4)
+
+	# Pattern6
+	# Turn off the Green LED pinout 7 & 22
+	for i in pattern6:
+		GPIO.output(i, GPIO.LOW)
+		
+	# Turn off the Green LED pinout 7 & 22
+	for i in pattern3:
+		GPIO.output(i, GPIO.LOW)
+
+	# Pattern3
+	# Turn on the Yellow LED pinout 5 & 24
+	# Turn on the Yellow LED pinout 10 & 21
+	for i in pattern3:
+		GPIO.output(i, GPIO.HIGH)
+
+	time.sleep(2)
+
+	# Pattern7
 	# Turn off the Red LEDs pinout 12 & 19
-	GPIO.output(12, GPIO.LOW)
-	GPIO.output(19, GPIO.LOW)
 	# Turn off the Yellow LED pinout 5 & 24
-	GPIO.output(5, GPIO.LOW)
-	GPIO.output(24, GPIO.LOW)
 	# Turn off the Yellow LED pinout 10 & 21
-	GPIO.output(10, GPIO.LOW)
-	GPIO.output(21, GPIO.LOW)
-	# Turn on the Red LED pinout 3 & 26
-	GPIO.output(3, GPIO.HIGH)
-	GPIO.output(26, GPIO.HIGH)
+
+	for i in pattern7:
+		GPIO.output(i, GPIO.LOW)
+
